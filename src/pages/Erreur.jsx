2c5f994e6a08ba400a17c11c57b6abbeec984ac6
Votre Nom <maeva.garcia13210@gmail.com>
2024-08-11 21:14:00 +0200

    rajout link to page accueil sur la page erreur

--- src/pages/Erreur.jsx
+++ src/pages/Erreur.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/erreur.scss'
+import { Link } from 'react-router-dom';
 
 function Erreur() {
   return (
     <div className='erreur-box'>
       <h1 className='erreur-titre'>404</h1>
       <p className='erreur-message'>Oups ! la page que vous demandez n'existe pas.</p>
+      <p>
+        <Link to="/" className='erreur-lien'>Retourner sur la page d'accueil</Link> 
+      </p>
     </div>
   );
 }
